5191701b1609ae6e55f49532724bd5137588c65f
Jacob Marks <jfmarks@udel.edu>
2023-02-09 14:58:04 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <header className="App">My name is Jacob Marks</header>
         </div>
     );
 }
