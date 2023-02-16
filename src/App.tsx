80203d68234b63444dcdbc3593421adc394f3b0f
Jacob Marks <jfmarks@udel.edu>
2023-02-15 20:34:30 -0500

    Added the required changes to App.css and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import "./App.css";
+import cat from "./assets/cat.png";
 
 function App(): JSX.Element {
     return (
@@ -11,8 +13,40 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <header className="App">My name is Jacob Marks</header>
-            <header className="App">Hello World</header>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="RedRec"></div>
+                        <ol>
+                            <li>Trapinch</li>
+                            <li>Flygon</li>
+                            <li>Vibrava</li>
+                            <li>Regirock</li>
+                        </ol>
+
+                        <Button
+                            onClick={() => console.log("Hello World!")}
+                            className="AppAlt"
+                        >
+                            Log Hello World
+                        </Button>
+                    </Col>
+                    <Col>
+                        <div className="RedRec"></div>
+                        <h1
+                            className="AppAlt"
+                            style={{ backgroundColor: "rgb(100,100,100)" }}
+                        >
+                            My name is Jacob Marks. Hello World. Flygon is a
+                            good Flygon is a good pokemon
+                        </h1>
+                        <img
+                            src={cat}
+                            alt="Nice opinion. One small issue. I am inside your home"
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
