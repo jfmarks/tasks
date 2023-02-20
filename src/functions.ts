201ae919b774ec3dd5798630669f5f5ce128b47a
Jacob Marks <jfmarks@udel.edu>
2023-02-20 11:55:31 -0500

    added the "!" to the shout function

--- src/functions.ts
+++ src/functions.ts
@@ -23,7 +23,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return message.toUpperCase();
+    return message.toUpperCase() + "!";
 }
 
 /**
